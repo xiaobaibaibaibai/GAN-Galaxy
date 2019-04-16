97986670fe356393adf7306f21a6c1af701fdb7c
wentian <wbai1295@gmail.com>
2019-04-16 16:50:11 -0400

    verion 1

--- all_galaxies.py
+++ all_galaxies.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
 
+import glob
 import os
 
 import numpy as np
@@ -16,20 +17,34 @@ mpl.rcParams['figure.figsize'] = np.array((10,6))*.6
 import misc
 import gan
 
+
 # Load galaxy image data
-X_img = np.load("data/images.small.npy")
+img_dir = "data/galaxy_images_training/npy_files/"
+filename_formatter = os.path.join(img_dir, "{}-cutout.npy")
+
+npy_files = glob.glob(filename_formatter.format("*"))
+
+HSC_ids = np.array([int(os.path.basename(f).split("-")[0])
+                    for f in npy_files])
+
+HSC_ids
+
+
+X_img = np.empty([len(HSC_ids), 3, 50, 50])
+for i, HSC_id in enumerate(HSC_ids):
+    X_img[i] = np.load(filename_formatter.format(HSC_id))
+
 X_img = X_img.transpose([0,2,3,1])
 X_img.shape
 
 image_size = X_img.shape[1]
 image_size
 
+
 # Load targets
-HSC_ids = np.load("data/HSC_ids.npy")
-HSC_ids
 
 df = pd.read_csv("data/2018_02_23-all_objects.csv")
-df = df[df.selected]
+# df = df[df.selected]
 
 
 df = df.drop_duplicates("HSC_id") \
@@ -41,8 +56,10 @@ df.head()
 y = df.loc[HSC_ids].values
 y_for_visualization_samples = np.array([.14, 8.51])
 
-standardizer = misc.Standardizer()
-standardizer.train(y)
+# values copied from output of `simple gan.ipynb`
+standardizer = misc.Standardizer(means = np.array([0.21093612, 8.62739865]),
+                                 std = np.array([0.30696933, 0.63783586]))
+# standardizer.train(y)
 print("means: ", standardizer.means)
 print("std:   ", standardizer.std)
 y_standard = standardizer(y)
@@ -50,9 +67,8 @@ y_for_visualization_samples_standard = standardizer(y_for_visualization_samples)
 
 y_standard.shape
 
-
 # Run GAN
-num_threads = 10
+num_threads = 4
 
 sess = tf.Session(config=tf.ConfigProto(
     intra_op_parallelism_threads=num_threads,
@@ -61,8 +77,7 @@ sess = tf.Session(config=tf.ConfigProto(
 
 train = True
 if train:
-    # num_epochs = 450
-    num_epochs = 10
+    num_epochs = 100
     # use a dir outside of dropbox
     checkpoint_dir = os.path.join(os.path.expanduser("~"),
                                   "tmp - models",
@@ -71,11 +86,12 @@ else:
     num_epochs = 1
     # use a dir inside the repo
     checkpoint_dir = "models/gan/checkpoints"
-
+    
 batch_size = 64
 z_dim = 100
-dataset_name = "galaxy"
-result_dir = "models/gan/results"
+dataset_name = "galaxy_all"
+# result_dir = "models/gan/results"
+result_dir = "models/gan"
 log_dir = "models/gan/log"
 
 model = gan.CGAN(sess, num_epochs, batch_size, z_dim, dataset_name,
@@ -90,8 +106,6 @@ model = gan.CGAN(sess, num_epochs, batch_size, z_dim, dataset_name,
 model.build_model()
 
 model.train()
-
-
 
 
 
